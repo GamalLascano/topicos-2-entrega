48a74cd4bc492b1b401cfd04a924733d33c5d43f
Gamal Lascano <gamallascano@gmail.com>
2025-02-08 17:40:51 -0300

    Initial additions, working mongo server and half working subscription manager service

---
+++ mongo-init/mongo-init.js
@@ -0,0 +1,13 @@
+db = db.getSiblingDB('subscriptions');
+db.createCollection('users');
+db.users.createIndex({apiKey: "text"}, {unique: true});
+db.users.insertMany([{
+    "apiKey": "3a30a7f7-5374-4ffc-b061-5aa3145ad4ce",
+    "subscription": "freemium"
+  },
+  {
+    "apiKey": "d10aa37e-d9bc-44e7-aba4-79feab1e822b",
+    "subscription": "premium"
+  }]);
+
+print('Database set up complete!');
---
+++ subscription_manager/main.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify
+import pymongo
+import os
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from bson.json_util import dumps
+
+app = Flask(__name__)
+
+class User():
+    def __init__(self,id:ObjectId, apiKey:str, subscription:str):
+        self._id = id
+        self.apiKey = apiKey
+        self.subscription = subscription
+
+def db_bean():
+    mongo_url = os.getenv('MONGO_URL', 'mongodb://localhost:27017/subscriptions')
+    client = MongoClient(mongo_url)
+    db = client["subscriptions"]
+    return db
+
+@app.route('/')
+def hello_world():
+    return 'Hello to my app'
+
+@app.route('/auth')
+def auth():
+    db = db_bean()
+    collection = db["users"]
+    users = collection.find({})
+    records = []
+    for record in users:
+        record['_id'] = str(record['_id'])  # Convert ObjectId to string for JSON serialization
+        records.append(record)
+    return jsonify(records)
+
+@app.route('/auth', methods=['POST'])
+def insert_user():
+    db = db_bean()
+    collection = db["users"]
+    result = collection.insert_one(request.get_json())
+    result.inserted_id
+    result2 = collection.find_one({"_id": result.inserted_id})
+    return dumps(result2), 201
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5004)
