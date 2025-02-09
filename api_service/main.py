d47ca1fd1772ab6be3be95982da71cfb233ca31b
Gamal Lascano <gamallascano@gmail.com>
2025-02-09 00:05:48 -0300

    Added cache and token validation to the service endpoint in api service

--- api_service/main.py
+++ api_service/main.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, request
+
+from subscription_manager.service import isValidToken
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World'
+
+@app.route('/service')
+def same_entity():
+    authToken = request.headers.get('Authorization')
+    if (authToken):
+        validation = isValidToken(authToken)
+        if validation.isValid:
+            return 'Request was validated!', 200
+        else:
+            return 'Invalid token', 403
+    else:
+        return 'No token provided', 401
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
---
+++ api_service/subscription_manager/service.py
@@ -0,0 +1,28 @@
+import requests
+import os
+from pymemcache.client import base
+
+MEMCACHED_HOST = os.getenv('MEMCACHED_HOST', 'localhost')
+MEMCACHED_PORT = int(os.getenv('MEMCACHED_PORT', 11211))
+
+def getCacheClient():
+    return base.Client((MEMCACHED_HOST, MEMCACHED_PORT))
+
+class Validation():
+    def __init__(self, isValid:bool, subscription:str):
+        self.isValid = isValid
+        self.subscription = subscription
+
+def isValidToken(apiKey: str):
+    cacheClient = getCacheClient()
+    cacheValue = cacheClient.get(apiKey)
+    if (cacheValue):
+        return Validation(True, cacheValue.decode('utf-8'))
+    else:
+        response = requests.get('http://subscription_manager:5004/auth?api_key=' + apiKey)
+        if response.status_code == 200:
+            json = response.json()
+            cacheClient.set(apiKey, json['subscription'])
+            return Validation(True, json['subscription'])
+        else:
+            return Validation(False, None)
